686b6c3a01b1ebcff7189ba50c45912bf358c2bf
Vindula Pahasarani <85609775+Vindulapahasarani@users.noreply.github.com>
2024-07-02 16:21:10 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,45 @@
+# Screen Size
+width = 800
+height = 450
+
+# Secondary screen size
+resolution = "480x320"
+
+# Background Images
+bgImage1 = "Images/BgImage_1.png"
+bgImage2 = "Images/BgImage_2.png"
+
+# Button images
+startBtn = "Images/StartBtn.png"
+setTimeBtn = "Images/SetTimeBtn.png"
+homeBtn = "Images/HomeBtn.png"
+
+# List of Time in minutes. You can modify it.
+timeList = [1, 3, 5, 7, 10]
+
+# Default inhale and exhale time in seconds. 
+# Try to keep it as it is. Inhale+Exhale = must be 10.
+inhaleTime = 5
+exhaleTime = 5
+
+# Default meditation time in seconds
+baseTime = 60
+
+# Font choices
+font1 = "Times new roman"
+font2 = "Helvetica"
+font3 = "Courier"
+font4 = "Kokila"
+
+# Color choices
+color1 = "#63afdb"
+color2 = "white"
+color3 = "black"
+color4 = "yellow"
+color5 = "green"
+color6 = "sky blue"
+
+# Voice Cover
+Relax = "Voices/Relax.mp3"
+Inhale = "Voices/Inhale.mp3"
+Exhale = "Voices/Exhale.mp3"
